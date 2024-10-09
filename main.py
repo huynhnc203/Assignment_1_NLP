d25d3316cfd1e9e87df91c9fef92b214acbf68b4
TruowNguyen <nguyentruonghk17@gmail.com>
2024-10-09 12:07:37 +0700

    test lai

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ from word2vec import vectorizer
 sentences = ["xin chào", "tạm biệt"]
 sentence_embeddings = vectorizer.transform(sentences)
 print(sentence_embeddings)
+
+nearest_word = vectorizer.k_nearest("xin chào", 5)
+print(nearest_word)
