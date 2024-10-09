203017e727e38153e13cc161944e0a41247fadb1
TruowNguyen <nguyentruonghk17@gmail.com>
2024-10-09 12:06:04 +0700

    test

--- word2vec/embedding_vectorizer.py
+++ word2vec/embedding_vectorizer.py
@@ -1,6 +1,8 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.metrics.pairwise import cosine_similarity
+from vector_operator.vector_dis import VectorOperator
+from vector_operator.vector_dis import CosineSimilarity
 import numpy as np
+
 
 class EmbeddingVectorizer(BaseEstimator, TransformerMixin):
     def __init__(self, embedding_dict):
@@ -17,21 +19,26 @@ class EmbeddingVectorizer(BaseEstimator, TransformerMixin):
         ])
 
     def k_nearest(self, sentence, k=5, metric='cosine'):
-        # Tính vector của câu cần so sánh
-        query_vec = np.mean([self.embedding_dict.get(word, np.zeros(self.embedding_dim)) for word in sentence.split()], axis=0)
+        "bai tap 2 :)) "
         
-        # Tính vector cho tất cả câu trong embedding_dict
-        all_sentences = list(self.embedding_dict.keys())  # Lấy danh sách các câu/từ trong từ điển
-        all_vecs = np.array([self.transform([sentence])[0] for sentence in all_sentences])  # Chuyển đổi tất cả các câu/từ thành vector
+        sentence_vec = self.transform([sentence])[0]
         
-        # Tính độ tương đồng cosine giữa vector của câu query và các vector khác
-        if metric == 'cosine':
-            similarities = cosine_similarity([query_vec], all_vecs)[0]  # Độ tương đồng cosine
-        
-        # Tìm k câu có độ tương đồng lớn nhất
-        top_k_indices = np.argsort(similarities)[-k:][::-1]  # Sắp xếp theo thứ tự giảm dần và lấy k câu gần nhất
-        
-        # Trả về các câu tương ứng với các chỉ số tìm được
-        return [all_sentences[i] for i in top_k_indices]
+        if sentence_vec is None:
+            print(f"Embedding for sentence '{sentence}' not found")
+            return []
+        similarities = []
+
+        cosine_calculator = CosineSimilarity()
+
+        for key, value in self.embedding_dict.items():
+            print(f"{key} {value}")
+            sim_score = cosine_calculator.calculate(sentence_vec, value)
+            similarities.append((key, sim_score))
+
+
+        similarities.sort(key=lambda x: x[1], reverse=True)
+
+        return [word for word, score in similarities[:k]]
+                
 
 
